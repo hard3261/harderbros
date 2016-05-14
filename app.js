034b1fe49f9d08c68aecbd3d3c98bc3488153713
Eric Harder <ericharder@Erics-MacBook-Pro.local>
2016-05-13 17:25:46 -0700

    app.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use(express.static(__dirname));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(80, function () {
+  console.log('App listening on port 80!');
 });
